eb069f64d97a88a91e9c887895f5848ec46493c8
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2020-10-25 08:18:11 +0100

    ✨ feat: query for posts

--- app/src/main/java/com/inventrohyder/parstagram/MainActivity.java
+++ app/src/main/java/com/inventrohyder/parstagram/MainActivity.java
@@ -1,14 +1,38 @@
 package com.inventrohyder.parstagram;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.parse.ParseQuery;
+
 public class MainActivity extends AppCompatActivity {
+
+    public final String TAG = getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        queryPosts();
+    }
+
+    private void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground((posts, e) -> {
+            if (e != null) {
+                Log.e(TAG, "done: Issue with getting posts", e);
+                return;
+            }
+
+            for (Post post : posts) {
+                Log.i(TAG,
+                        "Post desc: " + post.getDescription() + ", username: " + post.getUser().getUsername()
+                );
+            }
+        });
     }
 }
