a87013d9c6a8ce7d09728310506a9a04fda50f50
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2020-10-25 15:53:23 +0100

    ✨ feat: image capture and post creation

--- app/src/main/java/com/inventrohyder/parstagram/MainActivity.java
+++ app/src/main/java/com/inventrohyder/parstagram/MainActivity.java
@@ -1,20 +1,35 @@
 package com.inventrohyder.parstagram;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
 
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 0;
     public final String TAG = getClass().getSimpleName();
     private EditText mEtDescription;
+    public String photoFileName = "photo.jpg";
+    private File photoFile;
+    private ImageView mIvPostImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
 
         mEtDescription = findViewById(R.id.etDescription);
 
+        Button btnCaptureImage = findViewById(R.id.btnCaptureImage);
+        btnCaptureImage.setOnClickListener(view -> launchCamera());
+
         Button btnSubmit = findViewById(R.id.btnSubmit);
         btnSubmit.setOnClickListener(view -> {
             String description = mEtDescription.getText().toString();
@@ -30,17 +48,74 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Description cannot be empty", Toast.LENGTH_SHORT).show();
                 return;
             }
+            if (photoFile == null || mIvPostImage.getDrawable() == null) {
+                Toast.makeText(getApplicationContext(), "There is no image!", Toast.LENGTH_SHORT).show();
+                return;
+            }
             ParseUser currentUser = ParseUser.getCurrentUser();
-            savePost(description, currentUser);
+            savePost(description, currentUser, photoFile);
         });
 
 //        queryPosts();
     }
 
-    private void savePost(String description, ParseUser currentUser) {
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                mIvPostImage = findViewById(R.id.ivPostImage);
+                mIvPostImage.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void launchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(this, "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        return new File(mediaStorageDir.getPath() + File.separator + fileName);
+    }
+
+    private void savePost(String description, ParseUser currentUser, File photoFile) {
         Post post = new Post();
         post.setDescription(description);
-//        post.setImage();
+        post.setImage(new ParseFile(photoFile));
         post.setUser(currentUser);
         post.saveInBackground((e -> {
             if (e != null) {
@@ -49,6 +124,7 @@ public class MainActivity extends AppCompatActivity {
             }
             Log.i(TAG, "savePost: Post save was successful!!");
             mEtDescription.setText("");
+            mIvPostImage.setImageResource(0);
         }));
     }
 
